62005b90b4627081971c5c007fe4e8040e80c6cf
sam-astro <77079540+sam-astro@users.noreply.github.com>
2022-01-13 10:12:12 -0500

    VSync

--- Slang/graphics.h
+++ Slang/graphics.h
@@ -733,7 +733,7 @@ int initGraphics(std::string windowTitle, int width, int height)
 	TTF_Init();
 
 	gWindow = SDL_CreateWindow(windowTitle.c_str(), 40, 40, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-	gRenderer = SDL_CreateRenderer(gWindow, -1, 0, SDL_RENDERER_ACCELERATED);
+	gRenderer = SDL_CreateRenderer(gWindow, -1, 0, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
 	//Get window surface
 	gScreenSurface = SDL_GetWindowSurface(gWindow);
